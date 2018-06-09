145091db0d4277cb821a4d562ed98b9404bf544a
huy12121212 <huy12121212@gmail.com>
2018-06-09 17:27:04 +1000

    first

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require("express");
+const sql = require('mssql')
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+
+// Enable tcp protocol for sql server, set the sttaic port to 1433 for all ips, restart the server
+
+//const DbConnectionString = 'mssql://{user-name}:{password}@{db-server-ip}:{sql-server-port}/{db-name}';
+const DbConnectionString = 'mssql://sa:sa@169.254.123.91/Book';
+
+sql.on('error', err => {
+    console.dir(err);
+    sql.close();
+});
+var year = '2003';
+var y2 = '2012';
+
+//var pu = "\'% Wil %\'";
+var pu = "\'% Wil%\'";
+
+//where ProductDescription like '%A4%'//
+app.get("/", function(req, res) {
+    sql.connect(DbConnectionString).then(pool => {
+        return pool.request()
+           .query(`select * from Books where Publisher like ${pu} and Year = ${y2}`);
+           console.log(pu);
+            
+    }).then(result => {
+        sql.close();
+        res.render("data", { model: result.recordset });
+    }).catch(err => {
+        console.dir(err);
+        sql.close();
+    });
+});
+
+var port = '4150'
+app.listen(port, function() {
+    console.log("node server listening at port : " + port);
+});
